642bc3df50e2aec88e625276848b9ad49687f3a8
RpArt1 <rapczynski@gmail.com>
2024-09-07 09:47:23 +0200

    Refactoring

--- app/services/conversation_service.py
+++ app/services/conversation_service.py
@@ -14,27 +14,54 @@ from app.services import conversation_history_service
 async def reply_user(user_message:str, conversation_id:str, db: AsyncSession = Depends(get_db_session)) -> str:
     try: 
         logging.info(f"{conversation_id} ==> User posted message: {user_message}")
-        current_date = (datetime.now()).strftime("%Y-%m-%d")
+        
+        prompt = await build_prompt(user_message, conversation_id, db)
+      
+        response = open_ai_service.get_message_from_ai(prompt)
 
-        placeholders = {
-                "assistant_name" : "Xian",
-                "date" : current_date,
-                "user_name" : "Yan"
-        }
-        conversation_system_prompt = file_processor.process_file("../prompts/conversation_prompt.md", placeholders)
-        conversations_history = await conversation_history_service.fetch_conversations_by_uuid(conversation_id, db)
-
-        conversations_history.append({"role": "user", "content": user_message})
-        conversation_with_system_prompt = [{"role": "system", "content": conversation_system_prompt}] + conversations_history
-
-        response = open_ai_service.get_message_from_ai(conversation_with_system_prompt)
         await save_conversation(conversation_id, user_message, response, db)
+
         logging.info(f"{conversation_id} ==> Reply for user : {response}")
+
         return response
+    
     except ValueError as e:
         logging.error(f"Error occured when generation response to user {e}")
+
     except Exception as e:
         logging.error(f"Error occured when generation response to user {e}")
+
+
+async def build_prompt(user_message: str, conversation_id:str, db):
+
+    """Build prompt consisting of system prompt,  current query from user and if applicaple conversation history 
+
+    Args:
+        user_message (str): current message from user 
+        conversation_id (str): _description_
+        db (_type_): _description_
+    """
+    placeholders = {
+        "assistant_name" : "Xian",
+        "date" : (datetime.now()).strftime("%Y-%m-%d"),
+        "user_name" : "Yan"
+    }
+    system_prompt = file_processor.process_file("../prompts/conversation_prompt.md", placeholders)
+    
+    conversations_list = []
+
+    searched_conversations_list = await conversation_history_service.fetch_conversations_by_uuid(conversation_id, db)
+
+    if(len(searched_conversations_list) > 0 ): 
+
+        logging.info(f"Adding: {len(searched_conversations_list)} conversations to prompt")
+        conversations_list = searched_conversations_list
+
+    conversations_list.append({"role": "user", "content": user_message})
+
+    conversation_with_system_prompt = [{"role": "system", "content": system_prompt}] + conversations_list
+
+    return conversation_with_system_prompt
 
 
 async def save_conversation(conversation_id: str, user_message: str, response: str, db: AsyncSession ) -> None: 
