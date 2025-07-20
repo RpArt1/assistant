36a5104bcde52a2635ebd8c20b57640f9b077022
RpArt1 <rapczynski@gmail.com>
2025-07-20 11:57:38 +0200

    Update unit test for ai service

--- app/tests/unit/unit_ai_service_test.py
+++ app/tests/unit/unit_ai_service_test.py
@@ -5,15 +5,19 @@ import openai
 import logging
 from unittest.mock import patch, ANY
 
+
+@pytest.fixture
+def user_message():
+    return "What is artificial intelligence?"
+
 @patch('app.services.ai_service.call_ai')
-def test_classify_query(mock_call_ai):
-    user_message = "What is artificial intelligence?"
+def test_classify_query(mock_call_ai, user_message):
     excepted_value = {'type': 'query', 'tags': ['ai']}
 
    # Set the return value of the mocked call_ai function to simulate a real API response
     mock_call_ai.return_value = excepted_value
 
-    logging.info("Mock OpenAI API setup complete")
+    logging.debug("Mock OpenAI API setup complete")
 
     # Call the categorise_user_query function with the user message
     response = categorise_user_query(user_message, False)
@@ -22,3 +26,11 @@ def test_classify_query(mock_call_ai):
     logging.info(f"response for test call for user query categorisation is {response}")
 
     assert response == excepted_value
+
+
+@patch('app.services.ai_service.call_ai')
+def test(mock_call,user_message):
+    mock_call.return_value = None
+    response = categorise_user_query(user_message)
+    mock_call.assert_return_value = None 
+    assert response == None
